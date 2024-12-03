8e043d14353024d5fa481a44616745e44082a4e4
masoome_aghayari <masoome.aghayari@gmail.com>
2024-12-04 00:38:49 +0330

    SecurityConfigs added

---
+++ src/main/java/ir/azkivaam/banking_system/config/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package ir.azkivaam.banking_system.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author masoome.aghayari
+ * @since 12/1/24
+ */
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+@EnableWebMvc
+public class SecurityConfig {
+    private final JwtToAuthenticationTokenConvertor jwtAuthenticationConverter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.oauth2ResourceServer(t -> t.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(
+                    jwtAuthenticationConverter)))
+            .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .csrf(AbstractHttpConfigurer::disable)
+            .headers(AbstractHttpConfigurer::disable)
+            .cors(CorsConfigurer::disable);
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(HttpMethod.OPTIONS)
+                           .requestMatchers(HttpMethod.POST, "/api/banking-system/swagger-ui/index.html",
+                                            "/api/banking-system/v3/api-docs/**",
+                                            "/api/banking-system/swagger-ui/**",
+                                            "/api/banking-system/swagger-ui.html");
+    }
+
+}
+
