94a5e8862dffb61a8d5635fa18e25f550cf54f4c
masoome_aghayari <masoome.aghayari@gmail.com>
2024-12-04 00:39:14 +0330

    MessageSourceConfig.java added

---
+++ src/main/java/ir/azkivaam/banking_system/config/LocaleConfig.java
@@ -0,0 +1,12 @@
+package ir.azkivaam.banking_system.config;
+
+import java.util.Locale;
+
+/**
+ * @author masoome.aghayari
+ * @since 12/1/24
+ */
+
+public class LocaleConfig {
+    public static final Locale LOCALE = new Locale("fa_IR");
+}
---
+++ src/main/java/ir/azkivaam/banking_system/config/MessageSourceConfig.java
@@ -0,0 +1,25 @@
+package ir.azkivaam.banking_system.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+/**
+ * @author masoome.aghayari
+ * @since 12/1/24
+ */
+
+@Configuration
+public class MessageSourceConfig {
+
+    @Bean
+    public MessageSource messageSource(@Value("${application.encoding}") String encoding) {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding(encoding);
+        messageSource.setCacheSeconds(10);
+        return messageSource;
+    }
+}
